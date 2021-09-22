550a647fa493237e746dc38eeb410e8caa5b5b4b
abhinav.b.kumar <abhinav.b.kumar@pwc.com>
2021-09-22 07:16:09 +0530

    cyrrency pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name:string='';
   date:string=''
+  amount:number=0;
   onNameChange(event:Event){
       this.name=(event.target as HTMLInputElement).value;
   }
   onDateChange(event:Event){
     this.date=(event.target as HTMLInputElement).value;
+  }
+  onAmountChange(event:Event){
+    this.amount=parseFloat((event.target as HTMLInputElement).value);
   }
 }
