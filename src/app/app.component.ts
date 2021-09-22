d8121e8141500e538fc45305d7a97c14f2694806
abhinav.b.kumar <abhinav.b.kumar@pwc.com>
2021-09-22 06:54:55 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'payment-form';
+  name:string='';
+  date:string=''
+  onNameChange(event:Event){
+      this.name=(event.target as HTMLInputElement).value;
+  }
+  onDateChange(event:Event){
+    this.date=(event.target as HTMLInputElement).value;
+  }
 }
